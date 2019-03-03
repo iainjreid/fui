7e95f9a352350a465da474885df9ed8c4f5a4980
Iain Reid <iainreid382@gmail.com>
2019-03-03 18:42:01 +0000

    Add single import source for ease of use

---
+++ packages/fui-core/lib/fui-core.js
@@ -0,0 +1,17 @@
+"use strict";
+module.exports = new Proxy(() => undefined, {
+    get(obj, prop) {
+        return element(prop, {});
+    }
+});
+function element(tag, attrs) {
+    return Object.assign((content) => `<${tag}${attrs.length ? " " : ""}${Object.entries(attrs).map(([name, value]) => `${name}='${value}'`).join(" ")}>${content || ""}</${tag}>`, {
+        attrs(attrs) {
+            return element(tag, attrs);
+        },
+        promote(fn) {
+            return (scope) => fn(scope);
+        }
+    });
+}
+;
---
+++ packages/fui/lib/fui.js
@@ -0,0 +1,3 @@
+'use strict';
+
+exports.core = require("@chaff/fui-core");
