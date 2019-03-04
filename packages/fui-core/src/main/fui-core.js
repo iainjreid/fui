043a117746c77f766c60a1e7dfefa433131de953
Iain Reid <iainreid382@gmail.com>
2019-03-04 18:01:07 +0000

    Re-write templating engine to return DOM elements

--- packages/fui-core/src/main/fui-core.js
+++ packages/fui-core/src/main/fui-core.js
@@ -1,19 +1,24 @@
 "use strict";
 
-module.exports = new Proxy(() => undefined, {
-  get(obj, prop) {
-    return element(prop, {});
-  }
-});
+module.exports = ((Object) => ((builder, element, methods) => (
+	element = (args) =>
+		Object.assign(builder(args), methods(args)),
 
-function element(tag, attrs) {
-  return Object.assign((content) => `<${tag}${attrs.length ? " " : ""}${Object.entries(attrs).map(([name, value]) => `${name}='${value}'`).join(" ")}>${content || ""}</${tag}>`, {
-    attrs(attrs) {
-      return element(tag, attrs);
-    },
+	methods = ([tag, attrs, props]) => ({
+		attrs: (attrs) => element([tag, attrs, props]),
+		props: (props) => element([tag, attrs, props]),
 
-    promote(fn) {
-      return (scope) => fn(scope);
-    }
-  });
-};
+		promote: (fn) => (scope) => fn(scope)
+	}),
+
+	new Proxy(Object, {
+		get: (_, prop) => element([prop, {}, {}])
+	})
+))(([tag, attrs, props], element) => (element = document.createElement(tag), (content) => (
+	Object.entries(attrs).forEach(([key, value]) => element.setAttribute(key, value)),
+	Object.entries(props).forEach(([key, value]) => element[key] = value),
+
+	typeof content !== "object" && (content = document.createTextNode(content)),
+
+	element.appendChild(content), element
+))))(Object)
