c86dfa9a59186576cd9d9b76d98bde584a7ee327
Iain Reid <iainreid382@gmail.com>
2019-03-18 19:09:18 +0000

    Add initial fui-globals package

---
+++ packages/fui-globals/README.md
@@ -0,0 +1,4 @@
+# Fui Globals
+
+![npm](https://img.shields.io/npm/v/@chaff/fui-globals.svg?style=flat-square)
+![Bundlephobia](https://img.shields.io/bundlephobia/min/@chaff/fui-globals.svg?style=flat-square)
---
+++ packages/fui-globals/src/main/fui-globals.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const core = require("@chaff/fui-core");
+
+const elements = [
+  "a", "abbr", "address", "applet", "area", "article", "aside", "audio", "b", "base", "basefont", "bdo", "blockquote",
+  "br", "button", "canvas", "caption", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details",
+  "dfn", "dialog", "dir", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "font", "footer",
+  "form", "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "i", "iframe",
+  "img", "input", "ins", "kbd", "label", "legend", "li", "link", "map", "mark", "marquee", "menu", "meta", "meter",
+  "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q",
+  "rt", "ruby", "s", "samp", "script", "section", "select", "slot", "small", "source", "span", "strong", "style", "sub",
+  "sup", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "u",
+  "ul", "var", "video", "wbr"
+];
+
+exports.core = () => {
+  for (const element of elements) {
+    window[element] = core[element];
+  }
+}
