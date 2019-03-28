853a2e77e0adbc35e89e7b446eed07c9ea082139
Iain Reid <iainreid382@gmail.com>
2019-03-28 13:38:25 +0000

    Add tests for Core methods

--- packages/fui-core/src/test/fui-core.spec.ts
+++ packages/fui-core/src/test/fui-core.spec.ts
@@ -1,20 +1,78 @@
 import * as core from "../main/fui-core";
 
-class TestTarget {
-  constructor(public name: string, public children: TestTarget[] = []) { }
+interface FuiTestTarget {
+  name: string;
+  data?: string;
+  children: FuiTestTarget[];
 }
 
-const target = core<TestTarget, "foo">({
+interface FuiTestMethods {
+  data(str: string): this
+}
+
+const { foo, bar, baz } = core<FuiTestTarget, "foo" | "bar" | "baz", FuiTestMethods>({
   build(name: string) {
-    return new TestTarget(name)
+    return { name, children: [] }
   },
-  append(a: TestTarget, b: TestTarget) {
+  append(a: FuiTestTarget, b: FuiTestTarget) {
     return a.children.push(b)
   }
-});
+}, (tap: core.FuiTap<FuiTestTarget, void, FuiTestMethods>) => ({
+  data(str: string) {
+    return tap((_, fx) => fx.data = str);
+  }
+}));
+
+const checks = [
+  [foo(), {
+    name: "foo",
+    children: []
+  }],
+
+  [foo.add(bar)(), {
+    name: "foo",
+    children: [{
+      name: "bar",
+      children: []
+    }]
+  }],
+
+  [foo.add(bar.add(baz))(), {
+    name: "foo",
+    children: [{
+      name: "bar",
+      children: [{
+        name: "baz",
+        children: []
+      }]
+    }]
+  }],
+
+  [foo.lift((data: string) => bar.data(data))("data"), {
+    name: "foo",
+    children: [{
+      name: "bar",
+      data: "data",
+      children: []
+    }]
+  }],
+
+  [foo.lift((data: string) => bar.data(data).lift((data: string) => baz.data(data)))("data"), {
+    name: "foo",
+    children: [{
+      name: "bar",
+      data: "data",
+      children: [{
+        name: "baz",
+        data: "data",
+        children: []
+      }]
+    }]
+  }]
+]
 
 describe("fui-core", () => {
-  test("ensure tree is correct (1)", () => {
-    expect(target.foo()).toEqual(new TestTarget("foo"));
+  test.each(checks)("ensure tree is correct (%#)", (actual, extected) => {
+    expect(actual).toEqual(extected);
   });
 });
