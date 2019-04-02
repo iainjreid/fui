f00d4d44cedb829cd516bc12ff6b98b8fafeb44a
Iain Reid <iainreid382@gmail.com>
2019-04-02 11:58:44 +0100

    Fix "lift" and "scope" types
    Corrected the environment types for the lift and scope methods, and
    updated the tests accordingly.
    
    See: #15

--- packages/fui-core/src/main/fui-core.d.ts
+++ packages/fui-core/src/main/fui-core.d.ts
@@ -1,4 +1,4 @@
-declare function FuiCore<Type, Names extends PropertyKey, Methods = {}>(builder: FuiCore.FuiBuilder<Type, Names>, methods?: (tap: FuiCore.FuiTap<Type, Env, Methods>) => Methods): FuiCore.FuiEngine<Type, Names, Methods>;
+declare function FuiCore<Type, Names extends PropertyKey, Methods = {}>(builder: FuiCore.FuiBuilder<Type, Names>, methods?: (tap: FuiCore.FuiTap<Type, Methods>) => Methods): FuiCore.FuiEngine<Type, Names, Methods>;
 
 declare namespace FuiCore {
   type FuiEngine<Type, Names extends PropertyKey, Methods = {}> = Record<Names, FuiElement<Type, any, Methods> & Methods>;
@@ -8,14 +8,14 @@ declare namespace FuiCore {
     append(a: Type, b: Type): void
   }
 
-  type FuiTap<Type, Env, Methods> = (tap: (value: any, target: Type) => any) => FuiElement<Type, Env, Methods> & Methods;
+  type FuiTap<Type, Methods> = (tap: (value: any, target: Type) => any) => FuiElement<Type, unknown, Methods> & Methods;
 
   interface FuiElement<Type, Env, Methods> {
     (scope?: Env): Type;
 
     add: (element: this) => FuiElement<Type, Env, Methods> & Methods;
-    lift: <AltEnv = Env>(fn: (scope: AltEnv) => FuiElement<Type, AltEnv, Methods> & Methods) => FuiElement<Type, AltEnv, Methods> & Methods;
-    scope: <NewEnv>(fn: (scope: Env) => NewEnv) => FuiElement<Type, Env, Methods> & Methods;
+    lift: <AltEnv>(fn: (scope: AltEnv) => FuiElement<Type, AltEnv, Methods> & Methods) => FuiElement<Type, Env extends unknown ? AltEnv : Env, Methods> & Methods;
+    scope: <OldEnv>(fn: (scope: OldEnv) => Env) => FuiElement<Type, OldEnv, Methods> & Methods;
   }
 }
 
